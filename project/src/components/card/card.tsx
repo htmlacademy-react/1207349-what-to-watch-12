2ac65ca4035609ec3fd3a71f67f8d9be81c35b4b
cosmos <va-litv@yandex.ru>
2023-05-25 14:03:44 +0300

    add video player

--- project/src/components/card/card.tsx
+++ project/src/components/card/card.tsx
@@ -1,21 +1,42 @@
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import VideoPlayer from '../video-player/video-player';
+import { useEffect, useState } from 'react';
 
 type CardProps = {
   id: number;
   name: string;
   previewImage: string;
-  onMouseOver?: (id: number | null) => void;
+  previewVideoLink: string;
 }
 
-function Card({id, name, previewImage, onMouseOver}: CardProps): JSX.Element {
+function Card({id, name, previewImage, previewVideoLink}: CardProps): JSX.Element {
+  const [playVideo, setPlayVideo] = useState<boolean>(false);
+  const [cardMouseOver, setCardMouseOver] = useState<boolean>(false);
+
+  useEffect(()=> {
+    if (cardMouseOver) {
+      const timer = setTimeout(() => setPlayVideo(true), 1000);
+
+      return () => {
+        clearTimeout(timer);
+        setPlayVideo(false);
+      };
+    } else {
+      setPlayVideo(false);
+    }
+  }, [cardMouseOver]);
+
   return (
     <article
       className="small-film-card catalog__films-card"
-      onMouseOver={() => onMouseOver && onMouseOver(id)}
+      onMouseOver={() => setCardMouseOver(true)}
+      onMouseLeave={() => setCardMouseOver(false)}
     >
       <div className="small-film-card__image">
-        <img src={previewImage} alt={name} width="280" height="175" />
+        {playVideo
+          ? <VideoPlayer url={previewVideoLink} poster={previewImage} />
+          : <img src={previewImage} alt={name} width="280" height="175" /> }
       </div>
       <h3 className="small-film-card__title">
         <Link to={AppRoute.Film.replace(':id', id.toString())} className="small-film-card__link">{name}</Link>
