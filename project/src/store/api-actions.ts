84a7d9a49fb3e2f04246219bdc49c69193c97ba5
cosmos <va-litv@yandex.ru>
2023-05-26 16:16:12 +0300

    add new actions

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,9 +1,11 @@
 import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { AppDispatch, State } from '../types/state.js';
-import { loadFilms, setFilmsDataLoadingStatus } from './actions';
-import { APIRoute } from '../const';
-import { Films } from '../types/films.js';
+import { loadFilms, redirectToRoute, requireAuthorization, setFilmsDataLoadingStatus } from './actions';
+import { APIRoute, AppRoute, AuthStatus } from '../const';
+import { Films } from '../types/films';
+import { AuthData, UserData } from '../types/data';
+import { saveToken } from '../services/token';
 
 export const fetchFilmsAction = createAsyncThunk<void, undefined, {
   dispatch: AppDispatch;
@@ -16,5 +18,36 @@ export const fetchFilmsAction = createAsyncThunk<void, undefined, {
     const {data} = await api.get<Films>(APIRoute.Films);
     dispatch(setFilmsDataLoadingStatus(false));
     dispatch(loadFilms(data));
+  },
+);
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthStatus.NoAuth));
+    }
+  },
+);
+
+export const loginAction = createAsyncThunk<void, AuthData, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/login',
+  async ({login: email, password}, {dispatch, extra: api}) => {
+    const {data} = await api.post<UserData>(APIRoute.Login, {email, password});
+    saveToken(data.token);
+    // dispatch(loadUser(data));
+    dispatch(requireAuthorization(AuthStatus.Auth));
+    dispatch(redirectToRoute(AppRoute.Main));
   },
 );
