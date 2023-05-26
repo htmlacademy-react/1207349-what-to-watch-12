db25e30d9d5e131877a8dbac653228252f774c19
cosmos <va-litv@yandex.ru>
2023-05-26 16:19:06 +0300

    update login page

--- project/src/pages/login/login.tsx
+++ project/src/pages/login/login.tsx
@@ -1,21 +1,77 @@
+import { useNavigate } from 'react-router-dom';
 import Header from '../../components/header/header';
 import Layout from '../../components/layout/layout';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { AppRoute, AuthStatus } from '../../const';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
+import { AuthData } from '../../types/data';
+import { loginAction } from '../../store/api-actions';
 
 function Login(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const isAuth = useAppSelector((state) => state.authorizationStatus === AuthStatus.Auth);
+
+  const [formData, setFormData] = useState<AuthData>({
+    login: '',
+    password: '',
+  });
+
+  useEffect(() => {
+    if (isAuth) {
+      navigate(AppRoute.Main);
+    }
+  }, [isAuth, navigate]);
+
+  const fieldChangeHandler = (evt: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const {name, value} = evt.target;
+
+    if (name === 'password' && !/^(?=.*[a-zA-Z])(?=.*\d)(?=.{1,}$)/.test(value)) {
+      evt.target.setCustomValidity('Пароль должен состоять минимум из одной буквы и цифры.');
+    } else {
+      evt.target.setCustomValidity('');
+    }
+
+    setFormData({...formData, [name]: value});
+  };
+
+  const formSubmitHandler = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    dispatch(loginAction(formData));
+  };
+
   return (
     <Layout >
 
       <Header className="user-page__head" title='Sign in' />
 
       <div className="sign-in user-page__content">
-        <form action="#" className="sign-in__form">
+        <form action="#" className="sign-in__form" onSubmit={formSubmitHandler}>
           <div className="sign-in__fields">
             <div className="sign-in__field">
-              <input className="sign-in__input" type="email" placeholder="Email address" name="user-email" id="user-email" />
+              <input
+                onChange={fieldChangeHandler}
+                value={formData.login}
+                className="sign-in__input"
+                type="email"
+                name="login"
+                placeholder="Email"
+                required
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
             </div>
             <div className="sign-in__field">
-              <input className="sign-in__input" type="password" placeholder="Password" name="user-password" id="user-password" />
+              <input
+                onChange={fieldChangeHandler}
+                value={formData.password}
+                className="sign-in__input"
+                type="password"
+                name="password"
+                placeholder="Password"
+                minLength={2}
+                required
+              />
               <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
             </div>
           </div>
