47408238203bd997c45eacf5b235b11bfd243880
cosmos <va-litv@yandex.ru>
2023-05-31 19:16:55 +0300

    move film card to separate component

--- project/src/pages/film/film.tsx
+++ project/src/pages/film/film.tsx
@@ -1,17 +1,13 @@
 import { useParams } from 'react-router-dom';
 import NotFound from '../not-found/not-found';
-import Header from '../../components/header/header';
-import FilmInfo from '../../components/film-info/film-info';
 import Catalog from '../../components/catalog/catalog';
 import { RELATED_DISPLAY_COUNT } from '../../const';
-import FilmPoster from '../../components/film-poster/film-poster';
-import FilmBG from '../../components/film-bg/film-bg';
-import FilmTabs from '../../components/film-tabs/film-tabs';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { fetchFilmAction, fetchFilmReviewsAction, fetchSimilarFilmAction } from '../../store/api-actions';
 import { useEffect } from 'react';
 import { getFilm, getFilmReviews, getSimilarFilm } from '../../store/films-data/selectors';
 import PageContentLayout from '../../components/page-content-layout/page-content-layout';
+import FilmCard from '../../components/film-card/film-card';
 
 function Film(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -34,34 +30,7 @@ function Film(): JSX.Element {
 
   return (
     <>
-      <section className="film-card film-card--full">
-        <div className="film-card__hero">
-
-          <FilmBG filmPoster={film.backgroundImage} filmName={film.name} />
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <Header className="film-card__head" />
-
-          <div className="film-card__wrap">
-            <FilmInfo film={film} />
-          </div>
-        </div>
-
-        <div className="film-card__wrap film-card__translate-top">
-          <div className="film-card__info">
-
-            <FilmPoster
-              filmPoster={film.posterImage}
-              filmName={film.name}
-              className='film-card__poster--big'
-            />
-
-            <FilmTabs film={film} reviews={filmReviews} />
-
-          </div>
-        </div>
-      </section>
+      <FilmCard film={film} reviews={filmReviews} />
 
       <PageContentLayout title="More like this" className="catalog--like-this">
 
