1e028e14d2fbd650ed523e4d2cc8120dcf66e89f
cosmos <va-litv@yandex.ru>
2023-05-31 19:23:25 +0300

    move film card promo to separate component

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,16 +1,13 @@
 import { ALL_GENRES, CARD_DISPLAY_COUNT, GENRE_DISPLAY_COUNT } from '../../const';
-import Header from '../../components/header/header';
 import Catalog from '../../components/catalog/catalog';
 import Genres from '../../components/genres/genres';
-import FilmInfo from '../../components/film-info/film-info';
-import FilmPoster from '../../components/film-poster/film-poster';
-import FilmBG from '../../components/film-bg/film-bg';
 import { useAppSelector } from '../../hooks';
 import MoreButton from '../../components/more-button/more-butten';
 import { useState } from 'react';
 import { getFilms } from '../../store/films-data/selectors';
 import { getGenre } from '../../store/films-process/selectors';
 import PageContentLayout from '../../components/page-content-layout/page-content-layout';
+import FilmCardPromo from '../../components/film-card-promo/film-card-promo';
 
 
 function Main(): JSX.Element {
@@ -27,24 +24,7 @@ function Main(): JSX.Element {
 
   return (
     <>
-      <section className="film-card">
-
-        <FilmBG filmPoster={mainFilm.backgroundImage} filmName={mainFilm.name} />
-
-        <h1 className="visually-hidden">WTW</h1>
-
-        <Header className="film-card__head" />
-
-        <div className="film-card__wrap">
-          <div className="film-card__info">
-
-            <FilmPoster filmPoster={mainFilm.posterImage} filmName={mainFilm.name} />
-
-            <FilmInfo film={mainFilm} />
-
-          </div>
-        </div>
-      </section>
+      <FilmCardPromo film={mainFilm} />
 
       <PageContentLayout title="Catalog" hideTitle>
 
