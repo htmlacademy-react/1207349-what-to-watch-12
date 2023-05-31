77d16a876cbc311a63c650eacacd33137e4d214a
cosmos <va-litv@yandex.ru>
2023-05-31 11:59:48 +0300

    update tabs component

--- project/src/components/film-tabs/film-tabs.tsx
+++ project/src/components/film-tabs/film-tabs.tsx
@@ -5,13 +5,15 @@ import FilmDetails from '../film-details/film-details';
 import { Film } from '../../types/films';
 import { Tab } from '../../const';
 import TabLink from '../tab-link/tab-link';
+import { Comments } from '../../types/comments';
 
 type FilmTabsProps = {
   film: Film;
+  comments: Comments;
 }
 
-function FilmTabs({film}: FilmTabsProps): JSX.Element {
-  const [activeTab, setActiveTab] = useState<string>('Overview');
+function FilmTabs({film, comments}: FilmTabsProps): JSX.Element {
+  const [activeTab, setActiveTab] = useState<string>(Tab.Overview);
 
   return (
     <div className="film-card__desc">
@@ -35,7 +37,7 @@ function FilmTabs({film}: FilmTabsProps): JSX.Element {
           case Tab.Details:
             return <FilmDetails film={film} />;
           case Tab.Reviews:
-            return <FilmReviews />;
+            return <FilmReviews comments={comments} />;
         }
       }()}
     </div>
