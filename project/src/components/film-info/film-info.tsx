d1d45b0442f0087b792066d53c78bb91866ec892
cosmos <va-litv@yandex.ru>
2023-06-01 12:52:08 +0300

    add new func to player

--- project/src/components/film-info/film-info.tsx
+++ project/src/components/film-info/film-info.tsx
@@ -24,12 +24,12 @@ function FilmInfo({film}: FilmInfoProps): JSX.Element {
         <span className="film-card__year">{film.released}</span>
       </p>
       <div className="film-card__buttons">
-        <button className="btn btn--play film-card__button" type="button">
+        <Link to={AppRoute.Player.replace(':id', film.id.toString())} className="btn btn--play film-card__button" type="button">
           <svg viewBox="0 0 19 19" width="19" height="19">
             <use xlinkHref="#play-s"></use>
           </svg>
           <span>Play</span>
-        </button>
+        </Link>
         {isAuth &&
           <button className="btn btn--list film-card__button" type="button">
             <svg viewBox="0 0 19 20" width="19" height="20">
